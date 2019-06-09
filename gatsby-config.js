54a7ef69e0e8ab49a352cb375d1806e4ad4ca84a
Krasimir Nedelchev <19822240+kaykayehnn@users.noreply.github.com>
2019-06-09 20:19:54 +0300

    Setup Gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,42 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
+if (process.env.NODE_ENV !== "production") require("dotenv").config()
+
+// In your gatsby-config.js
 module.exports = {
-  /* Your site config here */
+  plugins: [
+    "gatsby-plugin-typescript",
+    /*
+     * Gatsby's data processing layer begins with “source”
+     * plugins. Here the site sources its data from Wordpress.
+     */
+    {
+      resolve: "gatsby-source-wordpress",
+      options: {
+        /*
+         * The base URL of the Wordpress site without the trailingslash and the protocol. This is required.
+         * Example : 'gatsbyjsexamplewordpress.wordpress.com' or 'www.example-site.com'
+         */
+        baseUrl: "megeto1.school.blog",
+        // The protocol. This can be http or https.
+        protocol: "https",
+        // Indicates whether the site is hosted on wordpress.com.
+        // If false, then the assumption is made that the site is self hosted.
+        // If true, then the plugin will source its content on wordpress.com using the JSON REST API V2.
+        // If your site is hosted on wordpress.org, then set this to false.
+        hostingWPCOM: true,
+        // If useACF is true, then the source plugin will try to import the Wordpress ACF Plugin contents.
+        // This feature is untested for sites hosted on wordpress.com.
+        // Defaults to true.
+        useACF: true,
+        auth: {
+          wpcom_app_clientSecret: process.env.WORDPRESS_CLIENT_SECRET,
+          wpcom_app_clientId: "65941",
+          wpcom_user: process.env.WORDPRESS_USER,
+          wpcom_pass: process.env.WORDPRESS_PASSWORD,
+        },
+      },
+    },
+  ],
 }
