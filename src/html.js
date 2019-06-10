9b1b20964013b93c0a25f609cce51a1fa606a371
Krasimir Nedelchev <19822240+kaykayehnn@users.noreply.github.com>
2019-06-11 01:37:28 +0300

    Modify html.js
    Add site icons, fix styles issues

---
+++ src/html.js
@@ -0,0 +1,84 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+const JSS_KEY = "jss-server-side"
+
+export default function HTML(props) {
+  const filterChildren = shouldMatchKey => child => {
+    const matchesKey = child && child.key === JSS_KEY
+    if (matchesKey !== shouldMatchKey) {
+      return null
+    }
+    return child
+  }
+
+  return (
+    <html {...props.htmlAttributes}>
+      <head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/icons/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/icons/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/icons/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link
+          rel="mask-icon"
+          href="/icons/safari-pinned-tab.svg"
+          color="#5bbad5"
+        />
+        <link rel="shortcut icon" href="/icons/favicon.ico" />
+        <meta name="msapplication-TileColor" content="#ffc40d" />
+        <meta name="msapplication-config" content="/icons/browserconfig.xml" />
+        <meta name="theme-color" content="#ffffff" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"
+        />
+        <title>Математическа Гимназия "Академик Кирил Попов"</title>
+        {/* This procedure is a workaround to fix CSS injection order. */}
+        {/* First are injected MUI styles, then custom ones. */}
+        {React.Children.map(props.headComponents, filterChildren(true))}
+        {React.Children.map(props.headComponents, filterChildren(false))}
+      </head>
+      <body {...props.bodyAttributes}>
+        {props.preBodyComponents}
+        <noscript key="noscript" id="gatsby-noscript">
+          This app works best with JavaScript enabled.
+        </noscript>
+        <div
+          key={`body`}
+          id="___gatsby"
+          dangerouslySetInnerHTML={{ __html: props.body }}
+        />
+        {props.postBodyComponents}
+      </body>
+    </html>
+  )
+}
+
+HTML.propTypes = {
+  htmlAttributes: PropTypes.object,
+  headComponents: PropTypes.array,
+  bodyAttributes: PropTypes.object,
+  preBodyComponents: PropTypes.array,
+  body: PropTypes.string,
+  postBodyComponents: PropTypes.array,
+}
