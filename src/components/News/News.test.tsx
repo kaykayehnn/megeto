f836ded7ffc1a0439074927aaea1226e46a1e424
Krasimir Nedelchev <19822240+kaykayehnn@users.noreply.github.com>
2019-06-11 12:19:33 +0300

    Delete tests
    ¯\_(ツ)_/¯

--- src/components/News/News.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import 'jest-dom/extend-expect'
-import { render } from '@testing-library/react'
-
-import News from '.'
-
-describe('News Component', () => {
-  it.todo('renders')
-})
--- src/components/NewsGrid/NewsGrid.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import 'jest-dom/extend-expect'
-import { render } from '@testing-library/react'
-
-import NewsGrid from '.'
-
-describe('News Component', () => {
-  it.todo('renders')
-})
