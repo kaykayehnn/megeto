b74a569f4a25bf717227277ef39bd7c7f2c2cf3f
Krasimir Nedelchev <19822240+kaykayehnn@users.noreply.github.com>
2019-06-11 13:12:03 +0300

    Update Layout test

--- src/components/Layout/Layout.test.tsx
+++ src/components/Layout/Layout.test.tsx
@@ -1,23 +1,25 @@
-import React from 'react'
-import { render } from '@testing-library/react'
+import React from "react"
+import { render } from "@testing-library/react"
 
-import 'jest-dom/extend-expect'
+import "jest-dom/extend-expect"
 
-import Layout from '.'
+import Layout from "."
 
 const headerText = 'Математическа Гимназия "Академик Кирил Попов"'
 
-describe('Layout Component', () => {
-  it('renders header when not on home page', () => {
-    const { getByText } = render(<Layout location='/example' />)
+describe("Layout Component", () => {
+  it("renders header when not on home page", () => {
+    const { getByText } = render(<Layout location={{ pathname: "/example" }} />)
 
     const header = getByText(headerText)
 
     expect(header).toBeInTheDocument()
   })
 
-  it('does not render header when on home page', () => {
-    const { queryByText } = render(<Layout location='/' />)
+  it("does not render header when on home page", () => {
+    const { queryByText } = render(
+      <Layout location={{ pathname: "/example" }} />
+    )
 
     const header = queryByText(headerText)
 
